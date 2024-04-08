dff949fe158ef1c4bd31b1b2bb32232535469764
ShreeramNew <shreerambca1@gmail.com>
2024-04-08 21:46:10 +0530

    practicing flex properties

--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import { View,StyleSheet } from 'react-native'
+import React from "react";
+import { View, StyleSheet } from "react-native";
 
 export default function Home() {
-  return (
-    <View style={[HomeStyle.container,HomeStyle.boxShadow]}> 
-      
-    </View>
-  )
+   return (
+      <View style={HomeStyle.container}>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={[HomeStyle.box, HomeStyle.meBox]}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={HomeStyle.box}></View>
+         <View style={[HomeStyle.box, HomeStyle.meBox]}></View>
+         <View style={[HomeStyle.box, HomeStyle.meBox]}></View>
+      </View>
+   );
 }
 
-const HomeStyle=StyleSheet.create({
-    container:{
-        width:200,
-        height:200,
-        backgroundColor:'blue',
-        marginTop:10
-    },
-    boxShadow:{
-        shadowColor:'purple',
-        shadowOffset:{
-            width:9,
-            height:10,
-        },
-        shadowOpacity:0,
-        shadowRadius:0,
-        elevation:10,
-    }
-})
+const HomeStyle = StyleSheet.create({
+   container: {
+      display: "flex",
+      width: 200,
+      height: 200,
+      backgroundColor: "blue",
+      marginTop: 10,
+      flexDirection: "row",
+      flexWrap: "wrap",
+      gap: 10,
+      justifyContent: "center",
+      alignContent: "center",
+   },
+   box: {
+      width: 20,
+      height: 20,
+      backgroundColor: "green",
+      borderWidth: 2,
+      borderColor: "white",
+      marginLeft: 4,
+   },
+   meBox:{
+    flex:1
+   }
+   
+});
